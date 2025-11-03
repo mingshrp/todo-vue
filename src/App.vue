c3d913670eb0e8d3ffcb556ecc27b01f8ed64bc8
mingshrp <mingmashrp23@gmail.com>
2025-11-03 13:22:33 -0700

    Add component to App

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
-<script setup></script>
+<script>
+import ToDoItem from "./components/ToDoItem.vue";
+
+export default {
+  name: "App",
+  components: {
+    ToDoItem,
+  },
+};
+</script>
 
 <template>
   <div id="app">
-    <h1>To-Do List</h1>
-  </div>
+  <h1>To-Do List</h1>
+  <ul>
+    <li>
+      <to-do-item></to-do-item>
+    </li>
+  </ul>
+</div>
 </template>
 
 <style scoped></style>
