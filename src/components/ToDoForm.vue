4b98bc1e68c84a220f3a4047332dd363ef520a24
mingshrp <mingmashrp23@gmail.com>
2025-11-10 10:06:40 -0700

    Add global styles to App and update form

--- src/components/ToDoForm.vue
+++ src/components/ToDoForm.vue
@@ -1,13 +1,18 @@
 <template>
-<form @submit.prevent="onSubmit">
-    <label for="new-todo-input"> What needs to be done? </label>
-     <input
-        type="text"
-        id="new-todo-input"
-        name="new-todo"
-        autocomplete="off"
-        v-model="label" />
-    <button type="submit">Add</button>
+  <form @submit.prevent="onSubmit">
+    <h2 class="label-wrapper">
+      <label for="new-todo-input" class="label__lg">
+        What needs to be done?
+      </label>
+    </h2>
+    <input
+      type="text"
+      id="new-todo-input"
+      name="new-todo"
+      autocomplete="off"
+      v-model.lazy.trim="label"
+      class="input__lg" />
+    <button type="submit" class="btn btn__primary btn__lg">Add</button>
   </form>
 </template>
 
