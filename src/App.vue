c9a5e055d7f3a0793fca378a7277141ea0a1008d
mingshrp <mingmashrp23@gmail.com>
2025-11-10 10:08:34 -0700

    Scoped Style ToDoItem component

--- src/App.vue
+++ src/App.vue
@@ -45,7 +45,7 @@ export default {
   <div id="app">
   <h1>To-Do List</h1>
   <to-do-form @todo-added="addToDo"></to-do-form>
- <ul>
+  <ul aria-labelledby="list-summary" class="stack-large">
       <li v-for="item in ToDoItems" :key="item.id">
         <to-do-item
           :label="item.label"
