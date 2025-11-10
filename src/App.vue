2e9a2e5ca05f9ab11bfef76b17f06390dfb8c77c
mingshrp <mingmashrp23@gmail.com>
2025-11-10 09:39:23 -0700

    add unique keys to todo items using nanoid

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,42 @@
 <script>
 import ToDoItem from "./components/ToDoItem.vue";
+import { nanoid } from "nanoid";
 
 export default {
-  name: "App",
+  name: "app",
   components: {
     ToDoItem,
   },
+  data() {
+    return {
+      ToDoItems: [
+        { id: `todo-${nanoid()}`, label: "Learn Vue", done: false },
+        {
+          id: `todo-${nanoid()}`,
+          label: "Create a Vue project with the CLI",
+          done: true,
+        },
+        { id: `todo-${nanoid()}`, label: "Have fun", done: true },
+        {
+          id: `todo-${nanoid()}`,
+          label: "Create a to-do list",
+          done: false,
+        },
+      ],
+    };
+  },
 };
+
 </script>
 
 <template>
   <div id="app">
   <h1>To-Do List</h1>
-  <ul>
-    <li>
-      <to-do-item label="My To-Do Item" :done="true"></to-do-item>
-
-    </li>
-  </ul>
+ <ul>
+  <li v-for="item in ToDoItems" :key="item.id">
+    <to-do-item :label="item.label" :done="item.done"></to-do-item>
+  </li>
+</ul>
 </div>
 </template>
 
